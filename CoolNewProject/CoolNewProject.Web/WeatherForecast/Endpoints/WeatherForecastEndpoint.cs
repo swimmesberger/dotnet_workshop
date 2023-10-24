71235e3fd1f9f6834ba03d6e9679e1927577b506
swimmesberger <wimmesberger@gmail.com>
2023-10-24 14:56:26 +0200

    Cleanup

--- CoolNewProject/CoolNewProject.Web/WeatherForecast/Endpoints/WeatherForecastEndpoint.cs
+++ CoolNewProject/CoolNewProject.Web/WeatherForecast/Endpoints/WeatherForecastEndpoint.cs
@@ -1,10 +1,8 @@
-﻿using CoolNewProject.Domain.WeatherForecast;
+﻿using System.Diagnostics.CodeAnalysis;
+using CoolNewProject.Domain.WeatherForecast;
 using CoolNewProject.Domain.WeatherForecast.Contracts;
 using CoolNewProject.Web.Endpoints;
 using Microsoft.AspNetCore.Mvc;
-// ReSharper disable once UnusedType.Global
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable UnusedType.Global
 
 namespace CoolNewProject.Web.WeatherForecast.Endpoints; 
 
@@ -13,6 +11,9 @@ namespace CoolNewProject.Web.WeatherForecast.Endpoints;
 /// It is best practice to use as many classes as possible when there are different requirements (read as DI services),
 /// to ensure that every endpoint is separately testable with as less dependencies as possible.
 /// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 public sealed class WeatherForecastEndpoint : IEndpointProvider {
     public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder app) {
         var root = app.MapGroup("/weatherforecast")
@@ -36,6 +37,7 @@ public sealed class WeatherForecastEndpoint : IEndpointProvider {
         }
 
         // inject scoped services in method: Services, token, HttpContext
+        // keep public for better testability
         public async Task<List<WeatherForecastDto>> HandleAsync([FromServices] IWeatherForecastService forecastService,
             CancellationToken cancellationToken) {
             return await forecastService.GetForecasts(cancellationToken);
@@ -48,6 +50,7 @@ public sealed class WeatherForecastEndpoint : IEndpointProvider {
         }
         
         // inject scoped services in method: Services, token, HttpContext
+        // keep public for better testability
         public async Task<IResult> HandleAsync([FromServices] IWeatherForecastService forecastService,
             [FromRoute] Guid id, CancellationToken cancellationToken) {
             return Results.Ok(await forecastService.GetForecastById(id, cancellationToken));
